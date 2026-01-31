e3c64417af399612066b860a49edc8b196cab0c4
bharvi0042becse25-byte <bharvi0042.becse25@chitkara.edu.in>
2026-01-31 21:16:28 +0530

    Add files via upload

--- 4th.c
+++ 4th.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main(){
     const float pi=3.14;
-    float r,area;
-    printf("Enter value of r");
+    float r,area,circumference;
+    printf("Enter value of r:");
     scanf("%f",&r);
     area=pi*r*r;          //area of circle=pi*r^2
-    printf("Area of radius %f is %f",r,area);
+    circumference=2*pi*r;
+    printf("Area of radius %.4f is %.4f",r,area);
+    printf("Circumference of radius %.4f is %.4f",r,circumference);
 }
